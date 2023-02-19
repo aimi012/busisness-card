6546c515773f2300fb6c33ac74f2593475c2d1b2
aimi012 <aimiamirul01@gmail.com>
2023-02-19 19:23:34 +0800

    uuu

--- client/src/info.jsx
+++ client/src/info.jsx
@@ -2,8 +2,8 @@ function Info() {
     return(
         <div className="info">
             <h3>Aimi Amirul</h3>
-            <h4>Front-End Developer</h4>
-            <h3>aimiamiurl.com</h3>
+            <h3>Front-End Developer</h3>
+            <h4>aimiamiurl.com</h4>
             <button>Email</button>
             <button>LinkedIn</button>
         </div>
